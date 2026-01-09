8a8c8881a123063e49378993b9fb9e092367b4e8
r-georg <rachelaimeegeorg@aol.com>
2026-01-09 20:13:44 +0100

    added uppercase stopword removal to keyword extraction

--- Semantische_Erschließung_Tests/keywords.py
+++ Semantische_Erschließung_Tests/keywords.py
@@ -19,8 +19,18 @@ stopword_file = join(base_dir, "data", "stopwords_keywords.txt")
 
 
 # Stopword list from .txt file
+# with open(stopword_file, "r", encoding="utf-8") as f:
+#    stopwords = [line.strip() for line in f]
+
 with open(stopword_file, "r", encoding="utf-8") as f:
-    stopwords = [line.strip() for line in f]
+    base_stopwords = {line.strip().lower() for line in f if line.strip()}
+
+stopwords_uppercase_lowercase = base_stopwords | {word.capitalize() for word in base_stopwords}
+
+stopwords = list(stopwords_uppercase_lowercase)
+
+print(stopwords)
+
 
 # Load multilingual model (supports German as one of the languages) for keyBERT
 kw_model = KeyBERT(model="paraphrase-multilingual-MiniLM-L12-v2")
@@ -37,17 +47,18 @@ def yake_keyword_extraction(text):
     # With custom parameters
     custom_kw_extractor = yake.KeywordExtractor(
         n=3,                   # ngram size
-        dedupLim=0.8,          # deduplication threshold
-        dedupFunc='levs',      # deduplication function
+        dedupLim=0.9,          # deduplication threshold
+        dedupFunc='seqm',      # deduplication function
         windowsSize=1,         # context window
         top=10,                # number of keywords to extract
-        features=None,         # custom features
         stopwords = set(stopwords)
     )
 
     keywords = custom_kw_extractor.extract_keywords(text)
 
-    return keywords
+    print(keywords)
+
+    return [tuple[0] for tuple in keywords] # Only return keywords, not score
 
 
 
@@ -56,12 +67,13 @@ def keybert_keyword_extraction(text):
 
     vectorizer = CountVectorizer(
         stop_words=stopwords,
-        ngram_range=(1, 3)
+        ngram_range=(1, 3),
+        lowercase=False
     )
 
     keywords = kw_model.extract_keywords(text, vectorizer=vectorizer, top_n=10)
 
-    return keywords
+    return [tuple[0] for tuple in keywords] # Only return keywords, not score
 
 
 def keybert_keyword_extraction_diverse(text):
@@ -69,12 +81,13 @@ def keybert_keyword_extraction_diverse(text):
 
     vectorizer = CountVectorizer(
         stop_words=stopwords,
-        ngram_range=(1, 2)
+        ngram_range=(1, 3),
+        lowercase=False
     )
 
     keywords = kw_model.extract_keywords(text, vectorizer=vectorizer, top_n=10, use_mmr=True, diversity=0.7)
 
-    return keywords
+    return [tuple[0] for tuple in keywords] # Only return keywords, not score
     
 
 
@@ -99,21 +112,26 @@ def main():
             yake_keywords = yake_keyword_extraction(text)
             yake_output_file = join(yake_output_folder, f"yake_brief{i}")
             with open(yake_output_file, "w", encoding="utf-8") as out_file:
-                out_file.write('\n'.join('%s %s' % tuple for tuple in yake_keywords))
+                #out_file.write('\n'.join('%s %s' % tuple for tuple in yake_keywords))
+                out_file.write('\n'.join('%s' % kw for kw in yake_keywords))
 
 
             print(f"Keywords extracted with KeyBERT for brief{i}")
             keybert_keywords = keybert_keyword_extraction(text)
             keybert_output_file = join(keybert_output_folder, f"keybert_brief{i}")
             with open(keybert_output_file, "w", encoding="utf-8") as out_file:
-                out_file.write('\n'.join('%s %s' % tuple for tuple in keybert_keywords))
+                #out_file.write('\n'.join('%s %s' % tuple for tuple in keybert_keywords))
+                out_file.write('\n'.join('%s' % kw for kw in keybert_keywords))
+
 
 
             print(f"Diverse keywords extracted with KeyBERT for brief{i}")
             keybert_keywords = keybert_keyword_extraction_diverse(text)
             keybert_output_file = join(keybert_output_folder, f"keybert_brief{i}_diverse")
             with open(keybert_output_file, "w", encoding="utf-8") as out_file:
-                out_file.write('\n'.join('%s %s' % tuple for tuple in keybert_keywords))
+                #out_file.write('\n'.join('%s %s' % tuple for tuple in keybert_keywords))
+                out_file.write('\n'.join('%s' % kw for kw in keybert_keywords))
+
 
             i += 1
 

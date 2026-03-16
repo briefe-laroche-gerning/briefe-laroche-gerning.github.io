a64f284a7862c1c151e547177757be308ac7a247
r-georg <rachelaimeegeorg@aol.com>
2026-03-16 18:59:38 +0100

    fixed Header Dropdowns

--- Webseite/briefedition/src/components/Header.vue
+++ Webseite/briefedition/src/components/Header.vue
@@ -1,83 +1,65 @@
 <template>
+  <!-- HINWEIS: Der Inhalt dieser Datei wurde mithilfe von GPT-5 überarbeitet. -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
 
-      <!-- Brand / Home (Router-Link statt <a>) -->
+      <!-- Brand / Home -->
       <router-link class="navbar-brand d-flex align-items-center" to="/">
         <img src="@/assets/logo.png" alt="Logo" width="100" height="100" class="me-2" />
         Startseite
       </router-link>
 
-      <!-- Mobile Toggler/Hamburgermenü in mobilder Ansicht -->
-      <button 
-        class="navbar-toggler" 
-        type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navbarSupportedContent"
-      >
+      <!-- Hamburger für mobile Ansicht -->
+      <button class="navbar-toggler" type="button" @click="menuOpen = !menuOpen">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        
-        <!-- Links -->
+      <!-- Navigation -->
+      <div class="collapse navbar-collapse" :class="{ show: menuOpen }">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <li class="nav-item">
-                <router-link to="/briefe" class="nav-link">
-                  Briefe
-                </router-link>
-            </li>
- <li class="nav-item dropdown">
-  <a
-    class="nav-link dropdown-toggle"
-    role="button"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    Register
-  </a>
+          <!-- Einfache Links -->
+          <li class="nav-item">
+            <router-link 
+              to="/briefe" 
+              class="nav-link" 
+              :class="{ active: $route.path === '/briefe' }"
+            >
+              Briefe
+            </router-link>
+          </li>
 
-  <ul class="dropdown-menu">
-    <li>
-      <router-link class="dropdown-item" to="/personenregister">Personen</router-link>
-    </li>
-    <li>
-      <router-link class="dropdown-item" to="/ortsregister">Orte</router-link>
-    </li>
-    <li>
-      <router-link class="dropdown-item" to="/werkregister">Werke</router-link>
-    </li>
-  </ul>
-</li>
+          <!-- Dropdown: Register -->
+          <li class="nav-item dropdown" 
+              @mouseenter="registerOpen = true" 
+              @mouseleave="registerOpen = false">
+            <a class="nav-link dropdown-toggle" href="#">Register</a>
+            <ul class="dropdown-menu" v-show="registerOpen">
+              <li><router-link class="dropdown-item" to="/personenregister">Personen</router-link></li>
+              <li><router-link class="dropdown-item" to="/ortsregister">Orte</router-link></li>
+              <li><router-link class="dropdown-item" to="/werkregister">Werke</router-link></li>
+            </ul>
+          </li>
 
- <li class="nav-item dropdown">
-  <a
-    class="nav-link dropdown-toggle"
-    role="button"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    Über diese Edition
-  </a>
+          <!-- Dropdown: Über diese Edition -->
+          <li class="nav-item dropdown"
+              @mouseenter="aboutOpen = true"
+              @mouseleave="aboutOpen = false">
+            <a class="nav-link dropdown-toggle" href="#">Über diese Edition</a>
+            <ul class="dropdown-menu" v-show="aboutOpen">
+              <li><router-link class="dropdown-item" to="/transkriptionsrichtlinien">Transkriptionsrichtlinien</router-link></li>
+              <li><router-link class="dropdown-item" to="/about">Über die Briefe</router-link></li>
+            </ul>
+          </li>
 
-  <ul class="dropdown-menu">
-    <li>
-      <router-link class="dropdown-item" to="/transkriptionsrichtlinien">Transkriptionsrichtlinien</router-link>
-    </li>
-    <li>
-      <router-link class="dropdown-item" to="/about">Über die Briefe</router-link>
-    </li>
-  </ul>
-</li>
+          <!-- Impressum -->
+          <li class="nav-item">
+            <router-link to="/impressum" class="nav-link" :class="{ active: $route.path === '/impressum' }">
+              Impressum
+            </router-link>
+          </li>
 
-  <li class="nav-item">
-    <router-link to="/impressum" class="nav-link">
-      Impressum
-    </router-link>
-  </li>
-  </ul>
-
+        </ul>
       </div>
     </div>
   </nav>
@@ -86,6 +68,13 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      menuOpen: false,       // Hamburger-Menü (mobile Ansicht) geöffnet / geschlossen
+      registerOpen: false,   // Dropdown Register
+      aboutOpen: false       // Dropdown Über diese Edition
+    };
+  }
 };
 </script>
 
@@ -95,35 +84,17 @@ export default {
 }
 
 .navbar {
-    background-color: #020664 !important;
+  background-color: var(--primary-blue) !important;
 }
 
+/* Dropdown sichtbar bei Hovern */
+.dropdown-menu {
+  display: block;
+  position: absolute;
+  visibility: hidden;
+}
+.nav-item.dropdown:hover .dropdown-menu {
+  visibility: visible;
+}
 
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
